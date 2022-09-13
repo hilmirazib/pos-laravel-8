9f6ca45e4d3b87c904fa65a65ebd6acf0081d1bf
hilmi.inolabs <hilmi.inolabs@gmail.com>
2022-09-13 21:45:05 +0700

    crud produk, delete multiple, cetak barcode

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produk;
+use App\Models\Kategori;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kategori = Kategori::all()->pluck('nama_kategori', 'id_kategori');
+        return view('produk.index', compact('kategori'));
+    }
+
+    public function data()
+    {
+        $produk = Produk::leftJoin('kategori', 'kategori.id_kategori', 'produk.id_kategori')
+            ->select('produk.*', 'nama_kategori')
+            ->orderBy('kode_produk', 'desc')
+            ->get();
+
+        return datatables()
+            ->of($produk)
+            ->addIndexColumn()
+            ->addColumn('select_all', fn ($produk) => '<input type="checkbox" name="id_produk[]" value="' . $produk->id_produk . '">')
+            ->addColumn('kode_produk', fn ($produk) => '<span class="label label-success">' . $produk->kode_produk . '</span>')
+            ->addColumn('harga_beli', fn ($produk) => format_rupiah($produk->harga_beli))
+            ->addColumn('harga_jual', fn ($produk) => format_rupiah($produk->harga_jual))
+            ->addColumn('stok', fn ($produk) => format_rupiah($produk->stok))
+            // ->addColumn('kategori', function ($produk) {
+            //     return $produk->nama_kategori;
+            // })
+            ->addColumn('aksi', function ($produk) {
+                return '
+                <div class="btn-group">
+                    <button onclick="editForm(`' . route('produk.update', $produk->id_produk) . '`)" class="btn btn-xs btn-info btn-flat"><i class="fa fa-pencil"></i></button>
+                    <button onclick="deleteData(`' . route('produk.destroy', $produk->id_produk) . '`)" class="btn btn-xs btn-danger btn-flat"><i class="fa fa-trash"></i></button>
+                </div>
+                ';
+            })
+            ->rawColumns(['aksi', 'kode_produk', 'select_all'])
+            ->make(true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $produk = Produk::latest()->first() ?? new Produk();
+        $request['kode_produk'] = 'P' . tambah_nol_didepan((int)$produk->id_produk + 1, 6);
+        $produk = Produk::create($request->all());
+        // $produk = new Produk();
+        // $produk->nama_produk = $request->nama_produk;
+        $produk->save();
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $produk = Produk::find($id);
+        return response()->json($produk);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $produk = Produk::find($id);
+        $produk->update($request->all());
+        return response()->json('Data berhasil disimpan', 200);
+        return response()->json('Data berhasil diupdate', 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $produk = Produk::find($id);
+        $produk->delete();
+
+        return response(null, 204);
+    }
+    public function deleteSelected(Request $request)
+    {
+        foreach ($request->id_produk as $id) {
+            $produk = Produk::find($id);
+            $produk->delete();
+        }
+
+        return response(null, 204);
+    }
+
+    public function cetakBarcode(Request $request)
+    {
+        $dataproduk = array();
+        foreach ($request->id_produk as $id) {
+            $produk = Produk::find($id);
+            $dataproduk[] = $produk;
+        }
+
+        $no  = 1;
+        $pdf = PDF::loadView('produk.barcode', compact('dataproduk', 'no'));
+        $pdf->setPaper('a4', 'potrait');
+        return $pdf->stream('produk.pdf');
+    }
+}
